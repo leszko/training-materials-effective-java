4b6541ef8d6027af61291a28c97e786bee081b27
leszko <rafal.leszko@gmail.com>
2015-08-01 20:55:20 +0200

    Initial commit

---
+++ src/main/java/com/luxoft/effectivejava/exercise1/User.java
@@ -0,0 +1,9 @@
+package com.luxoft.effectivejava.exercise1;
+
+public class User {
+    private final String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/com/luxoft/effectivejava/exercise1/UserTest.java
@@ -0,0 +1,5 @@
+package com.luxoft.effectivejava.exercise1;
+
+public class UserTest {
+
+}
