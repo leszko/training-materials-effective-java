057bdb0a91b9df05473391958f2a38eddca9b2da
leszko <rafal.leszko@gmail.com>
2015-08-01 22:14:27 +0200

    Add exercise 2.

---
+++ src/main/java/com/luxoft/effectivejava/exercise2/NutritionFacts.java
@@ -0,0 +1,52 @@
+package com.luxoft.effectivejava.exercise2;
+
+public class NutritionFacts {
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder {
+        private int calories = 0;
+        private int fat = 0;
+        private int carbohydrate = 0;
+        private int sodium = 0;
+
+        public Builder calories(int val) {
+            calories = val;
+            return this;
+        }
+
+        public Builder fat(int val) {
+            fat = val;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(Builder builder) {
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public int getFat() {
+        return fat;
+    }
+
+    public int getSodium() {
+        return sodium;
+    }
+
+    public int getCarbohydrate() {
+        return carbohydrate;
+    }
+}
