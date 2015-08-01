dcd4cb416e7160c6caf19afd3bf276bd99ae9e25
leszko <rafal.leszko@gmail.com>
2015-08-01 23:21:59 +0200

    Add exercise 3

---
+++ src/main/java/com/luxoft/effectivejava/exercise3/MemoryLeak.java
@@ -0,0 +1,10 @@
+package com.luxoft.effectivejava.exercise3;
+
+/**
+ * Create a memory leak that will cause java.lang.OutOfMemoryError
+ */
+public class MemoryLeak {
+    public void foo() {
+
+    }
+}
