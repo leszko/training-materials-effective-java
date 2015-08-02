e9634774beb085f31b7959c21f33e8db2fcbef7e
leszko <rafal.leszko@gmail.com>
2015-08-02 22:40:47 +0200

    Add exercise 6.

---
+++ src/main/java/com/luxoft/effectivejava/exercise6/NullRandomizer.java
@@ -0,0 +1,27 @@
+package com.luxoft.effectivejava.exercise6;
+
+import java.util.Random;
+
+public class NullRandomizer {
+
+    private final Random random;
+
+    public NullRandomizer(Random random) {
+        this.random = random;
+    }
+
+
+    public Integer randomizeIntegerIfNull(Integer n) {
+        if (n == null) {
+            return random.nextInt();
+        }
+        return n;
+    }
+
+    public Double randomizeDoubleIfNull(Double n) {
+        if (n == null) {
+            return random.nextDouble();
+        }
+        return n;
+    }
+}
